fee69f802d8980f12d0f6b0068cb871b0bc72a66
Dat Tieu <dat@catex.se>
2022-07-07 20:09:10 +0700

    add package headlessui, nuxt-icon

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-	<div>
+	<div class="bg-gray-100 min-h-screen">
 		<NuxtPage />
 	</div>
 </template>
+
+<style>
+@import '~/assets/css/main.css';
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/proxy'],
+	modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/proxy', 'nuxt-icons'],
 	proxy: {
 		'/api': {
 			target: 'http://localhost:8055',

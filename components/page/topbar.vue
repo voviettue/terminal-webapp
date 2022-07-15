07f557cd3e6aee2fd81a067d872d6fec87639647
Dat Tieu <dat@catex.se>
2022-07-15 13:58:48 +0700

    update topbar

--- components/page/topbar.vue
+++ components/page/topbar.vue
@@ -1,12 +1,22 @@
 <template>
 	<Disclosure v-slot="{ open }" as="nav" class="bg-white shadow">
-		<div class="mx-auto px-4 sm:px-6 lg:px-6">
+		<div class="mx-auto px-8">
 			<div class="flex justify-between h-16">
 				<div class="flex">
+					<div class="flex items-center sm:hidden">
+						<!-- Mobile menu button -->
+						<DisclosureButton
+							class="mr-4 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+						>
+							<span class="sr-only">Open main menu</span>
+							<nuxt-icon v-if="!open" class="h-6 w-6" name="menu" />
+							<nuxt-icon v-else class="h-6 w-6" name="OutlineX" />
+						</DisclosureButton>
+					</div>
 					<div class="flex-shrink-0 flex items-center">
 						<img
 							class="block lg:hidden h-8 w-auto"
-							src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+							src="/img/logo.png"
 							alt="Workflow"
 						/>
 						<img
@@ -43,17 +53,9 @@
 						</a>
 					</div>
 				</div>
-				<div class="hidden sm:ml-6 sm:flex sm:items-center">
-					<button
-						type="button"
-						class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>
-						<span class="sr-only">View notifications</span>
-						<nuxt-icon name="OutlineBell" fill class="h-6 w-6" />
-					</button>
-
+				<div class="flex items-center">
 					<!-- Profile dropdown -->
-					<Menu as="div" class="ml-3 relative">
+					<Menu as="div" class="relative">
 						<div>
 							<MenuButton
 								class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -78,7 +80,7 @@
 								class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
 							>
 								<MenuItem v-slot="{ active }">
-									<a
+									<!-- <a
 										href="#"
 										:class="[
 											active ? 'bg-gray-100' : '',
@@ -86,18 +88,15 @@
 										]"
 									>
 										Your Profile
-									</a>
-								</MenuItem>
-								<MenuItem v-slot="{ active }">
-									<a
-										href="#"
+									</a> -->
+									<NuxtLink
 										:class="[
 											active ? 'bg-gray-100' : '',
 											'block px-4 py-2 text-sm text-gray-700',
 										]"
 									>
-										Settings
-									</a>
+										Your Profile
+									</NuxtLink>
 								</MenuItem>
 								<MenuItem v-slot="{ active }">
 									<a
@@ -113,16 +112,6 @@
 							</MenuItems>
 						</transition>
 					</Menu>
-				</div>
-				<div class="-mr-2 flex items-center sm:hidden">
-					<!-- Mobile menu button -->
-					<DisclosureButton
-						class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-					>
-						<span class="sr-only">Open main menu</span>
-						<nuxt-icon v-if="!open" class="h-6 w-6" name="menu" />
-						<nuxt-icon v-else class="h-6 w-6" name="close" />
-					</DisclosureButton>
 				</div>
 			</div>
 		</div>
@@ -158,51 +147,6 @@
 				>
 					Calendar
 				</DisclosureButton>
-			</div>
-			<div class="pt-4 pb-3 border-t border-gray-200">
-				<div class="flex items-center px-4">
-					<div class="flex-shrink-0">
-						<img
-							class="h-10 w-10 rounded-full"
-							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-							alt=""
-						/>
-					</div>
-					<div class="ml-3">
-						<div class="text-base font-medium text-gray-800">Tom Cook</div>
-						<div class="text-sm font-medium text-gray-500">tom@example.com</div>
-					</div>
-					<button
-						type="button"
-						class="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>
-						<span class="sr-only">View notifications</span>
-						<nuxt-icon name="trash" class="h-6 w-6" aria-hidden="true" />
-					</button>
-				</div>
-				<div class="mt-3 space-y-1">
-					<DisclosureButton
-						as="a"
-						href="#"
-						class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-					>
-						Your Profile
-					</DisclosureButton>
-					<DisclosureButton
-						as="a"
-						href="#"
-						class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-					>
-						Settings
-					</DisclosureButton>
-					<DisclosureButton
-						as="a"
-						class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-						@click="logout()"
-					>
-						Sign out
-					</DisclosureButton>
-				</div>
 			</div>
 		</DisclosurePanel>
 	</Disclosure>

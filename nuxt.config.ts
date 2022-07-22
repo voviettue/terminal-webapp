be4047df6a9c671d32bdd6bc4bce1768411127fe
Dat Tieu <dat@catex.se>
2022-07-22 09:29:18 +0700

    add package pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	ssr: false,
-	modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/proxy', 'nuxt-icons'],
+	modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', 'nuxt-icons'],
+	buildModules: ['@pinia/nuxt'],
 	runtimeConfig: {
 		public: {
 			url: 'http://localhost:3000',

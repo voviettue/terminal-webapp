c66616667d65ac3a3e930b3ccd8c9bc0cb7c056c
Thien Nguyen <tatthien.contact@gmail.com>
2022-08-03 15:41:43 +0700

    [Debug] bypass api cache

--- server/middleware/proxy.ts
+++ server/middleware/proxy.ts
@@ -13,6 +13,8 @@ export default function (req: IncomingMessage, res: ServerResponse, next) {
 	const prefix = '/api'
 	if (req.url.startsWith(prefix)) {
 		req.url = req.url.replace(prefix, config.public.api.baseUrl)
+		req.headers['cache-control'] = 'no-store'
+
 		proxy.web(req, res)
 	} else {
 		next()

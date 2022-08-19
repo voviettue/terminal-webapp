27e7b32792caebc89c32e79ed5b08ae827d0b895
Dat Tieu <dat@catex.se>
2022-08-19 15:30:14 +0700

    add font roboto, lato, nato sans

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
 	<Html>
 		<Head>
+			<Link rel="preconnect" href="https://fonts.googleapis.com" />
+			<Link rel="preconnect" href="https://fonts.gstatic.com" />
 			<Link
 				rel="stylesheet"
-				href="https://fonts.googleapis.com/css?family=Lato:400,700"
+				href="https://fonts.googleapis.com/css?family=Roboto:400,700&family=Lato:400,700&family=Noto+Sans:400,700"
 			/>
 		</Head>
 

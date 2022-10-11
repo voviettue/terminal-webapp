7444d655d80d847463f6201b856bb6e40e713a0b
Dat Tieu <dat@catex.se>
2022-10-11 09:22:48 +0700

    handle option custom css for setting

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,23 +8,38 @@
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
+
 const route = useRoute()
 const { useUserStore, useSettingStore, usePageStore } = useStore()
 const { hydrate: userHydrate } = useUserStore()
 const { hydrate: pageHydrate } = usePageStore()
-const { hydrate: settingHydrate } = useSettingStore()
-
+const settingsStore = useSettingStore()
+const { settings } = storeToRefs(settingsStore)
 useHead({
 	title: `${route.meta.title}`,
 })
 
+const customCss = ref('')
 await init()
 
 async function init() {
 	try {
-		await Promise.all([userHydrate(), settingHydrate(), pageHydrate()])
+		await Promise.all([userHydrate(), settingsStore.hydrate(), pageHydrate()])
+		customCss.value = settings.value?.options?.custom_css
+		addCustomCss()
 	} catch (err) {
 		// do nothing
 	}
+}
+
+function addCustomCss() {
+	if (!customCss.value) return
+
+	const head = document.head
+	const style = document.createElement('style')
+	style.type = 'text/css'
+	style.appendChild(document.createTextNode(customCss.value))
+	head.append(style)
 }
 </script>

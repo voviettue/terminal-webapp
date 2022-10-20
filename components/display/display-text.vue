44cd1a1b0b9607a4216c847e86fb728f664ddd9c
Dat Tieu <dat@catex.se>
2022-10-20 13:43:33 +0700

    [Display][Text] support value as array

--- components/display/display-text.vue
+++ components/display/display-text.vue
@@ -1,19 +1,36 @@
+<!-- eslint-disable vue/no-template-shadow -->
+<!-- eslint-disable vue/no-multiple-template-root -->
 <template>
-	<WidgetText :widget="widget" />
+	<WidgetText
+		v-for="(value, index) in values"
+		:key="uniqueId('display-text-' + index)"
+		:widget="getWidget(value)"
+	/>
 </template>
 <script setup lang="ts">
+import uniqueId from 'lodash/uniqueId'
+
 interface Props {
 	value: any
 	options?: any
 }
 const props = defineProps<Props>()
-const text = Array.isArray(props.value) ? props.value.join('\n') : props.value
-const options = { ...props.options, text }
-const widget = {
-	id: 'display-text',
-	name: 'Display Text',
-	width: 'full',
-	options,
-	...options,
+const displayContext = inject('display-context')
+
+const values = computed(() => {
+	const text = props.options?.text || props.value
+	return Array.isArray(text) ? text : [text]
+})
+
+function getWidget(text) {
+	const options = { ...props.options, text }
+	return {
+		id: 'display-text',
+		name: 'Display Text',
+		width: 'full',
+		context: displayContext,
+		options,
+		...options,
+	}
 }
 </script>

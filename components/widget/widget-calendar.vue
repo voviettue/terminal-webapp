c69abd7b227d601f068487b0992d21427ec97059
Lộc Lê <loc@catex.se>
2022-10-20 16:00:42 +0700

    [Widget][Calendar][KeyValue] fix css

--- components/widget/widget-calendar.vue
+++ components/widget/widget-calendar.vue
@@ -87,7 +87,7 @@
 											active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
 											'block px-4 py-2 text-sm',
 										]"
-										@click="action('today')"
+										@click.prevent="action('today')"
 									>
 										Go to today
 									</a>

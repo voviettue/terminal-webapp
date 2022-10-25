aa06ebc223f5f953df0642acece1a69f5db33934
Dat Tieu <dat@catex.se>
2022-10-25 12:47:14 +0700

    import font Inter

--- app.vue
+++ app.vue
@@ -1,14 +1,6 @@
 <template>
 	<Html>
-		<Head>
-			<Link rel="preconnect" href="https://fonts.googleapis.com" />
-			<Link rel="preconnect" href="https://fonts.gstatic.com" />
-			<Link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css?family=Roboto:400,700&family=Lato:400,700&family=Noto+Sans:400,700"
-			/>
-		</Head>
-
+		<!-- <Head></Head> -->
 		<Body class="min-h-screen bg-zinc-50 text-sm text-zinc-800">
 			<NuxtLayout>
 				<NuxtPage />

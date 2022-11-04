ce731a02724c8eb75a5472009f808c64f49921d3
Huy Bui <huy@catex.se>
2022-11-04 17:03:52 +0700

    [Widget][Menu Button] Fix styles

--- components/widget/widget-menu-button.vue
+++ components/widget/widget-menu-button.vue
@@ -6,26 +6,26 @@
 					:class="buttonClass"
 					:style="styles"
 				>
-					<div v-if="leftIcon" class="pr-2 text-xl flex items-center">
-						<TwIcon :name="leftIcon" class="pr-2 h-max opacity-70"></TwIcon>
-					</div>
-					<template v-if="text">
+					<template v-if="text || leftIcon || rightIcon">
+						<div v-if="leftIcon" class="pr-2 text-xl flex items-center">
+							<TwIcon :name="leftIcon" class="pr-2 h-max opacity-70"></TwIcon>
+						</div>
 						{{ text }}
+						<div v-if="rightIcon" class="pl-2 text-xl flex items-center">
+							<TwIcon :name="rightIcon" class="pl-2 h-max opacity-70"></TwIcon>
+						</div>
 					</template>
 					<template v-else>
 						<div class="text-xl flex items-center">
 							<TwIcon name="menu" class="h-max opacity-70"></TwIcon>
 						</div>
 					</template>
-					<div v-if="rightIcon" class="pl-2 text-xl flex items-center">
-						<TwIcon :name="rightIcon" class="pl-2 h-max opacity-70"></TwIcon>
-					</div>
 				</MenuButton>
 			</div>
 
 			<transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
 				<MenuItems
-					:class="`absolute right-0 z-20 mt-2 w-56 origin-top-right bg-white shadow-${shadow} focus:outline-none`"
+					:class="`absolute right-0 z-20 mt-2 w-56 origin-top-right bg-white border border-gray-100 py-1.5 shadow-${shadow} focus:outline-none`"
 					:style="styles"
 				>
 					<MenuItem
@@ -36,15 +36,15 @@
 					>
 						<button
 							:class="[
-								active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-								'flex text-left px-4 py-2 text-sm w-full',
-								item.divider ? 'border-b border-b-gray-100' : ''
+								active ? 'bg-gray-100' : '',
+								'flex text-left px-4 py-2 w-full',
+								item.divider ? 'border-b border-b-gray-100' : '',
+								item.disabled ? 'opacity-40' : '',
 							]"
-							:style="styles"
 							@click="onButtonClick(item.onClick)"
 						>
-							<div v-if="item.icon" class="pr-2 text-xl flex items-center">
-								<TwIcon :name="item.icon" class="pr-2 h-max opacity-70"></TwIcon>
+							<div v-if="item.icon" class="text-xl flex items-center">
+								<TwIcon :name="item.icon" class="pr-3 h-max opacity-70"></TwIcon>
 							</div>
 							{{ item.label }}
 						</button>
@@ -73,7 +73,7 @@ const {
 	buttonVariant = 'primary',
 	position = 'center',
 	buttonSize = 'md',
-	shadow = 'sm',
+	shadow,
 	disabled,
 	buttonType = 'solid',
 	tooltip,
